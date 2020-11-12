8da7ff71377d30c90b03aff0239bb997e44c2c2f
Akiyamasouta <akiakiskyhand@gmail.com>
2020-11-12 22:16:47 +0900

    0と9がない正数列の個数計算(10~9+7)で割る

---
+++ ABC178-C.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#define rep(i, N) for(int i = 0; i < N; ++i)
+using namespace std;
+using cor = pair<int, int>;
+typedef long long ll;
+const ll mod = 1000000007;
+
+ll powmod(ll x, ll y) {
+    ll res = 1;
+    for(ll i=0; i<y; i++) {
+        res = res*x % mod;
+    }
+    return res;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    //0が一つもない
+    ll ans = powmod(10, n) - powmod(9, n) - powmod(9, n) + powmod(8, n);
+    //負の場合の対策(正なら何も変わらない)
+    ans = (ans + mod) % mod;
+    cout << ans << endl;
+}
