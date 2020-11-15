8924393f3d6277db5e91ec51491c1d1cdcd7ac2b
Akiyamasouta <akiakiskyhand@gmail.com>
2020-11-15 23:17:29 +0900

    Relu

---
+++ ABC183-A.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#define rep(i, N) for(int i = 0; i < N; ++i)
+using namespace std;
+using cor = pair<int, int>;
+typedef long long ll;
+const ll mod = 1000000007;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n < 0) {
+        cout << '0' << endl;
+    }
+    else {
+        cout << n << endl;
+    }
+}
