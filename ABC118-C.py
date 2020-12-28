83bc11f1f24d84f6af42813c2656d8b3f9416034
Akiyama Souta <akiakiskyhand@gmail.com>
2020-12-28 23:05:18 +0900

    最大公約数

---
+++ ABC118-C.py
@@ -0,0 +1,10 @@
+import math
+from functools import reduce
+
+def gcd_list(numbers):
+    return reduce(math.gcd, numbers)
+
+n = int(input())
+a = list(map(int, input().split()))
+
+print(gcd_list(a))
